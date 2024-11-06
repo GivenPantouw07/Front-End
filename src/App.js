6e32bec03a952898a68f14048ad9c78ce609d853
GivenPantouw07 <givenazarya@gmail.com>
2024-11-06 09:13:32 +0800

    updated material 03/11 and 06/11

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import Hello from "./components/Hello";
+import Users from "./components/Users";
 import World from "./components/World";
+import {useState} from 'react'
 
 const App = () => {
-  const hello = "Hello World";
+  // let hello = "Hello World";
+  const [hello, setHello] = useState("Selamat Datang"); //Nilai default
+
+  const handleClick = () => {
+    // hello = "Welcome";
+    setHello ("Welcome")
+    console.log(hello)
+  };
+
   return (
     /*JSX*/
     <div>
       <h1>{hello}</h1>
       <h2>Hello React</h2>
-      <Hello />
+      <Hello text="Hello 1" number ={20}/>
+      <Hello text="Hello 2" number ={30}/>
       <World />
+      <Users />
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,9 +1,9 @@
-const Hello = () => {
-    return (
-      <>
-        <h1>Component Hello</h1>
-      </>
-    );
-  };
+const Hello = ({ text, number }) => {
+  return (
+    <>
+      <h1>{`${text} ${number}`}</h1>
+    </>
+  );
+};
 
 export default Hello;
